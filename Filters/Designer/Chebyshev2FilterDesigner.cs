72459fd7d758e112ab71d91af6fa8e322cbebfdb
VrezenStrijder <f94fbr@outlook.com>
2025-05-29 22:00:44 +0800

    add filter designer

---
+++ Filters/Designer/Chebyshev2FilterDesigner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Filtering.IIR;
+using MathNet.Filtering.TransferFunctions;
+
+namespace DSPPlus.Filters
+{
+    public static class Chebyshev2FilterDesigner
+    {
+        /// <summary>
+        /// 简化二阶 Chebyshev2 滤波器
+        /// </summary>
+        public static ChebyshevFilter DesignLowPass(double cutoffFreq, double sampleRate, int order = 2, double stopDb = 40)
+        {
+            // 示例系数（使用 Python scipy.signal.cheby2(2, 40, 0.3)）
+            double[] b = { 0.3913, 0.0, -0.3913 };
+            double[] a = { 1.0, -0.3695, 0.2173 };
+
+            return new ChebyshevFilter(b, a);
+        }
+    }
+
+}
---
+++ Filters/Designer/EllipticFilterDesigner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPPlus.Filters
+{
+    public static class EllipticFilterDesigner
+    {
+        /// <summary>
+        /// 简化二阶 Elliptic 高通滤波器
+        /// </summary>
+        public static EllipticFilter DesignHighPass(double cutoffFreq, double sampleRate, double rippleDb = 1, double stopDb = 40, int order = 2)
+        {
+            // 示例系数，使用 Python scipy.signal.ellip(2, 1, 40, Wn=0.1, btype='high', analog=False, output='ba')
+            double[] b = { 0.7984, -1.5968, 0.7984 };
+            double[] a = { 1.0000, -1.5610, 0.6414 };
+
+            return new EllipticFilter(b, a);
+        }
+
+        /// <summary>
+        /// 简化 二阶Elliptic 低通滤波器
+        /// </summary>
+        public static EllipticFilter DesignLowPass(double cutoffFreq, double sampleRate, double rippleDb = 1, double stopDb = 40, int order = 2)
+        {
+            // 示例 Python: scipy.signal.ellip(2, 1, 40, 0.1) 生成的系数
+            double[] b = { 0.0675, 0.1195, 0.0675 };
+            double[] a = { 1.0000, -1.1430, 0.4128 };
+
+            return new EllipticFilter(b, a);
+        }
+    }
+
+}
